80be65925db00d2ac7339a8a017e7387bc936af1
eddikane <edwardkane@byui.edu>
2025-09-22 11:47:21 -0600

    Number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1; 
+            int guessCount = 0;
+
+            Console.WriteLine("\nI'm thinking of a number between 1 and 100...");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine()!);
+
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {guessCount} tries!");
+                }
+            }
+
+            Console.Write("\nDo you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine()!;
+        }
+
+        Console.WriteLine("Thanks for playing! Goodbye.");
     }
 }
