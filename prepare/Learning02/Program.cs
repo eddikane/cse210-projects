5653222d2491127fde7208fc6de905a83e5ccdc5
eddikane <edwardkane@byui.edu>
2025-10-02 12:27:21 -0600

    created a resume & job .cs file and populated the learning2 file

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
+        //Second Job
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+        //Resume
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        // Add the jobs to the resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();       
     }
 } 
---
+++ prepare/Learning02/resume.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic; // Needed for List<Job>
+
+public class Resume
+{
+    //Member Variables
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    // ----- Method -----
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
